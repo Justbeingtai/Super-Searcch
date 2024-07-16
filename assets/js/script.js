2219492a3638a878f9d3079a293b35270f395b1e
CJ <cjthompson562@gmail.com>
2024-07-15 21:34:18 -0500

    Update some files

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,8 +6,6 @@ const heroComicEl = $('hero-comic');
 
 function createHeroCard(hero) {
 
-
-
     const heroHeader = $('<div>')
         .addClass('')
         .text(hero.name);
@@ -34,6 +32,56 @@ function createHeroCard(hero) {
     heroImgEl.append(heroImg);
     heroComicEl.append(heroComicList);
 
+}
+
+function createWikiCard(wiki) {
+
+    const wikiList = $('<ul>');
+
+    const wikiTitles = wiki[1];
+    const wikiDescrip = wiki[2];
+    const wikiLinks = wiki[3];
+    
+
+    if (wikiTitles.length > maxWikiLength) {
+        wikiTitles.splice(maxWikiLength);
+        wikiDescrip.splice(maxWikiLength);
+        wikiLinks.splice(maxWikiLength);
+    }
+
+    for(i = 0; i<maxWikiLength; i++){
+        let title = wikiTitles[i];
+        let descrip = wikiDescrip[i];
+        let link = wikiLinks[i];
+
+        const wikiWrapper = $('<ul>')
+        .append($('<li>').text(title))
+        .append($('<li>').text(descrip))
+        .append($('<a>').attr('href', link)
+                .text(link));
+
+        wikiList.append(wikiWrapper);
+    }
+
+    wikiListEl.append(wikiList);
+
+}
+var url = "https://en.wikipedia.org/w/api.php"; 
+
+var params = {
+    action: "opensearch",
+    search: "Hampi",
+    limit: "5",
+    namespace: "0",
+    format: "json"
+};
+
+function getApi(url){
+    fetch(url).then(function(response){
+        console.log(response);
+    });
+        
+    
 }
 
 
